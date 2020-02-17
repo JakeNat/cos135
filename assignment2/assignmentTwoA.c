1206f6eec9658c638429c3f37ad2308e171c3dce
JakeNat <50184492+JakeNat@users.noreply.github.com>
2020-02-16 22:03:24 -0500

    Add files via upload

---
+++ assignment2/assignmentTwoA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("Name\tAge\tCountry\n");
+	printf("------------------------\n");
+	printf("Peter\t23\tUSA\n");
+	printf("Manuel\t43\tGermany\n");
+	printf("Ken\t35\tChina\n");
+	printf("Moz\t44\tGhana\n");
+	return 0;
+}
---
+++ assignment2/assignmentTwoB.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	char chr = 67;
+	printf("Character having ASCII value 67 is %d.\n", chr);
+	return 0;
+}
