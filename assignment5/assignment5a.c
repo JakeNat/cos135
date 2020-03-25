4378f97afc59b5c8861ac086f9f67ad3f42bc3c0
JakeNat <50184492+JakeNat@users.noreply.github.com>
2020-03-24 20:31:27 -0400

    Add files via upload

---
+++ assignment5/assignment5a.c
@@ -0,0 +1,30 @@
+// Name: Jake Natalizia
+// Date: March 1, 2020
+// Description: Checks if an inputted letter is a vowel or a consonant
+
+#include <stdio.h>
+
+int main() {
+	char input;
+
+	while (input != '-') {
+		printf("Enter a letter (press \'-\' to quit): ");
+		scanf(" %c", &input);
+
+		
+		if ((input > 'a' && input < 'e') || (input > 'e' && input < 'i') || (input > 'i' && input < 'o') || (input > 'o' && input < 'u')) {
+			printf("%c is a consonant.\n",input);
+		}
+		else  if ((input == 'A' || input == 'a') || (input == 'E' || input == 'e') || (input == 'I' || input == 'i') || (input == 'O' || input == 'o') || (input == 'U' || input == 'u')){
+			printf("%c is a vowel.\n",input);
+		}
+		else if (input == '-') {
+			break;
+		}
+		else {
+			printf("%c is not a letter in the alphabet.\n",input);
+		}
+		continue;
+	}
+	return 0;
+}
---
+++ assignment5/assignment5b.c
@@ -0,0 +1,23 @@
+// Name: Jake Natalizia
+// Date: March 1, 2020
+// Description: Finds sum of all even numbers between 1 and an inputted number
+
+#include <stdio.h>
+
+int main() {
+	int input = 0;
+	
+	printf("Input a number larger than 1: ");
+	scanf("%d",&input);
+
+	if (input > 1) {
+		int i, sum = 0;
+
+		for (i = 2; i <= input; i+=2) {
+			sum += i;
+		}
+		printf("Sum of even numbers between 1 and %d: %d",input,sum);
+	}
+	
+	return 0;
+}
